9bdb18ae9af276b23ff90d6adc1394c4ef1016c4
Saad Chraibi <saadchraibi85@gmail.com>
2023-08-04 11:49:24 +0100

    Second part done

---
+++ part1/src/App.js
@@ -0,0 +1,48 @@
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header title={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+const Header = (props) => {
+  return <h1>{props.title}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name}: {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  const totalExercises =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
+  return <p>Number of exercises: {totalExercises}</p>;
+};
+
+export default App;
